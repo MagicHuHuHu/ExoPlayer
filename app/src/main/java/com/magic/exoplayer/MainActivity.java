59f04ad2261a05dc66d5b3a29f36e6172da87249
magic <745979749@qq.com>
2019-09-19 22:25:12 +0800

    no message

---
+++ app/src/main/java/com/magic/exoplayer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.magic.exoplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author Magic
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
